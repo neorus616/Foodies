4f67aab1db7cbc9ac952f7c9a8c44eb7a1087001
CKazakov <Constantine.Kazakov@harman.com>
2018-12-29 15:26:43 +0200

    added follow/unfollow function

--- app/src/main/java/com/ariel/ckazakov/foodies/ProfileActivity.java
+++ app/src/main/java/com/ariel/ckazakov/foodies/ProfileActivity.java
@@ -3,8 +3,12 @@ package com.ariel.ckazakov.foodies;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,8 +25,9 @@ public class ProfileActivity extends AppCompatActivity {
 
     private TextView fullname;
     private CircleImageView profilePic;
+    private Button followButton, unfollowButton;
 
-    private DatabaseReference userRef;
+    private DatabaseReference userRef, followRef;
     private FirebaseAuth firebaseAuth;
 
     private String currentUserUid, userKey;
@@ -34,20 +39,52 @@ public class ProfileActivity extends AppCompatActivity {
 
         fullname = findViewById(R.id.profile_fullname_public);
         profilePic = findViewById(R.id.profile_pic_public);
+        followButton = findViewById(R.id.follow_button);
+        unfollowButton = findViewById(R.id.unfollow_button);
 
         if (getIntent().getExtras() != null)
             userKey = Objects.requireNonNull(getIntent().getExtras().get("userKey")).toString();
         firebaseAuth = FirebaseAuth.getInstance();
         currentUserUid = Objects.requireNonNull(firebaseAuth.getCurrentUser()).getUid();
-        if (userKey != null)
+
+        /*
+        other profiles
+         */
+        if (userKey != null && !userKey.equals(currentUserUid)) {
             userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userKey);
-        else
+            followRef = FirebaseDatabase.getInstance().getReference().child("Follows");
+            followRef.child(currentUserUid).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists())
+                        if (dataSnapshot.child(userKey).child("Follow").getValue() != Boolean.TRUE) {
+                            followButton.setVisibility(View.VISIBLE);
+                            unfollowButton.setVisibility(View.INVISIBLE);
+                        } else {
+                            followButton.setVisibility(View.INVISIBLE);
+                            unfollowButton.setVisibility(View.VISIBLE);
+                        }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+        /*
+        my profile
+         */
+        } else {
             userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserUid);
+            followButton.setVisibility(View.INVISIBLE);
+            unfollowButton.setVisibility(View.INVISIBLE);
+        }
 
         userRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()){
+                if (dataSnapshot.exists()) {
                     String firstname = Objects.requireNonNull(dataSnapshot.child("firstname").getValue()).toString();
                     String lastname = Objects.requireNonNull(dataSnapshot.child("lastname").getValue()).toString();
                     String profileImage = Objects.requireNonNull(dataSnapshot.child("profileimage").getValue()).toString();
@@ -63,5 +100,51 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                followButton.setVisibility(View.INVISIBLE);
+                FollowPerson();
+            }
+        });
+
+        unfollowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                followButton.setVisibility(View.VISIBLE);
+                UnfollowPerson();
+            }
+        });
+
+    }
+
+    private void UnfollowPerson() {
+        followRef.child(currentUserUid).child(userKey).child("Follow").setValue(Boolean.FALSE).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                followRef.child(userKey).child(currentUserUid).child("Follower").setValue(Boolean.FALSE).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        unfollowButton.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        });
+    }
+
+    private void FollowPerson() {
+        followRef.child(currentUserUid).child(userKey).child("Follow").setValue(Boolean.TRUE).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    followRef.child(userKey).child(currentUserUid).child("Follower").setValue(Boolean.TRUE).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            unfollowButton.setVisibility(View.VISIBLE);
+                        }
+                    });
+                }
+            }
+        });
     }
 }
